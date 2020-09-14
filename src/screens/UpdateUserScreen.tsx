a932cb243681ffc7ed55e7e93bc7362271f51e1c
kosaku kurino <kousaku.maron@gmail.com>
2020-09-14 12:00:59 +0900

    feat: add update logic to update user screen

--- src/screens/UpdateUserScreen.tsx
+++ src/screens/UpdateUserScreen.tsx
@@ -1,12 +1,16 @@
-import React, { useCallback, useState, useMemo, useLayoutEffect } from 'react'
+import React, { useCallback, useState, useMemo, useLayoutEffect, useEffect } from 'react'
 import { AntDesign } from '@expo/vector-icons'
-import { View, TouchableOpacity, Text, StyleSheet, Image, TextInput } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet, Image, TextInput, Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/core'
 import { useRoute } from '@react-navigation/native'
+import { UpdateUser } from '../entities/User'
+import { updateUser } from '../repositories/user'
+import { pickImageFromDevice, convertURLToBlob } from '../services/image'
 import { useUser } from '../services/hooks/user'
 import Avatar from '../components/atoms/avatar'
 import Separator from '../components/atoms/separator'
 import Spacer from '../components/atoms/spacer'
+import LoadingModal from '../components/moleculars/loadingModal'
 
 const coverImageURL =
   'https://image.shutterstock.com/z/stock-photo-friendly-romantic-encounter-boys-and-girls-autumn-day-in-the-old-town-363511535.jpg'
@@ -18,6 +22,32 @@ const UpdateUserScreen = () => {
   const uid = (route.params as any).uid
   const [user] = useUser(uid)
   const [newThumbnailURL, setNewThumbnailURL] = useState<string | null>(null)
+  const [name, setName] = useState<string>('')
+  const [profile, setProfile] = useState<string>('')
+  const [fetching, setFetching] = useState<boolean>(false)
+
+  const onPressAvatar = useCallback(async () => {
+    const uri = await pickImageFromDevice()
+    setNewThumbnailURL(uri)
+  }, [])
+
+  const save = useCallback(
+    async (name: string, profile: string, thumbnailURL: string) => {
+      if (!user) return
+      try {
+        setFetching(true)
+        const thumbnailBlob = await convertURLToBlob(thumbnailURL)
+        const data: UpdateUser = { name, profile, thumbnailBlob }
+        await updateUser(uid, data)
+        setFetching(false)
+        navigation.goBack()
+      } catch (e) {
+        setFetching(false)
+        Alert.alert('プロフィールの変更に失敗しました')
+      }
+    },
+    [navigation, uid, user]
+  )
 
   const showThumbnailURL = useMemo(() => {
     if (newThumbnailURL) {
@@ -34,12 +64,12 @@ const UpdateUserScreen = () => {
   const renderHeaderRight = useCallback(() => {
     return (
       <View style={styles.headerRightButtonWrapper}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => save(name, profile, showThumbnailURL)}>
           <Text style={styles.headerRightButtonText}>保存</Text>
         </TouchableOpacity>
       </View>
     )
-  }, [])
+  }, [name, profile, save, showThumbnailURL])
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -47,50 +77,68 @@ const UpdateUserScreen = () => {
     })
   }, [navigation, renderHeaderRight])
 
+  useEffect(() => {
+    if (!user) return
+    setName(user.name)
+    setProfile(user.profile)
+  }, [user])
+
   return (
-    <View style={styles.root}>
-      <Image style={styles.coverImage} source={{ uri: coverImageURL }} />
+    <React.Fragment>
+      <View style={styles.root}>
+        <Image style={styles.coverImage} source={{ uri: coverImageURL }} />
 
-      <View style={styles.headSection}>
-        <View style={styles.thumbnailWrapper}>
-          <Avatar size="l" uri={showThumbnailURL} />
+        <View style={styles.headSection}>
+          <View style={styles.thumbnailWrapper}>
+            <Avatar size="l" uri={showThumbnailURL} />
+          </View>
+          <View style={styles.cameraIconWrapper}>
+            <TouchableOpacity style={styles.cameraIconButton} onPress={onPressAvatar}>
+              <AntDesign name="camerao" color="#f8f8f8" size={36} />
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.cameraIconWrapper}>
-          <TouchableOpacity style={styles.cameraIconButton}>
-            <AntDesign name="camerao" color="#f8f8f8" size={36} />
-          </TouchableOpacity>
+
+        <Spacer size="xxl" />
+        <Separator />
+        <Spacer size="s" />
+
+        <View style={styles.section}>
+          <View style={styles.labelWrapper}>
+            <Text style={styles.labelText}>名前</Text>
+          </View>
+          <View style={styles.valueWrapper}>
+            <TextInput
+              style={styles.nameInput}
+              defaultValue={user && user.name}
+              onChangeText={(text) => setName(text)}
+            />
+          </View>
         </View>
+
+        <Spacer size="s" />
+        <Separator />
+        <Spacer size="s" />
+
+        <View style={styles.section}>
+          <View style={styles.labelWrapper}>
+            <Text style={styles.labelText}>自己紹介</Text>
+          </View>
+          <View style={styles.valueWrapper}>
+            <TextInput
+              style={styles.profileInput}
+              defaultValue={user && user.profile}
+              onChangeText={(text) => setProfile(text)}
+              multiline={true}
+            />
+          </View>
+        </View>
+
+        <Spacer size="s" />
+        <Separator />
       </View>
-
-      <Spacer size="xxl" />
-      <Separator />
-      <Spacer size="s" />
-
-      <View style={styles.section}>
-        <View style={styles.labelWrapper}>
-          <Text style={styles.labelText}>名前</Text>
-        </View>
-        <View style={styles.valueWrapper}>
-          <TextInput style={styles.nameInput} defaultValue={user && user.name} />
-        </View>
-      </View>
-
-      <Spacer size="s" />
-      <Separator />
-      <Spacer size="s" />
-
-      <View style={styles.section}>
-        <View style={styles.labelWrapper}>
-          <Text style={styles.labelText}>自己紹介</Text>
-        </View>
-        <View style={styles.valueWrapper}>
-          <TextInput style={styles.profileInput} defaultValue={user && user.profile} multiline={true} />
-        </View>
-      </View>
-
-      <Spacer size="s" />
-      <Separator />
-    </View>
+      <LoadingModal isVisible={fetching} />
+    </React.Fragment>
   )
 }
 
