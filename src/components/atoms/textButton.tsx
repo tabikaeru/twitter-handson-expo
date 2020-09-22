815e0a04eba89596bee23cc4bf804c7ccc1a8f4e
kosaku kurino <kousaku.maron@gmail.com>
2020-09-22 15:04:43 +0900

    feat: add fab and buttons components

---
+++ src/components/atoms/textButton.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { TouchableOpacity, Text, TouchableOpacityProps, StyleSheet } from 'react-native'
+
+type TextButtonProps = TouchableOpacityProps & {
+  text?: string
+  fontSize?: number
+  color?: string // html color code
+}
+
+const TextButton: React.FC<TextButtonProps> = ({
+  text,
+  fontSize = 12,
+  color = '#1da1f2',
+  style,
+  children,
+  ...rest
+}) => {
+  return (
+    <TouchableOpacity style={[styles.root, style]} {...rest}>
+      {text && <Text style={{ color, fontSize }}>{text}</Text>}
+      {children}
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+  },
+})
+
+export default TextButton
