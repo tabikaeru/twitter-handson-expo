b7ce4b6d50bf156a9e183791ddf11ee549cad6d5
kosaku kurino <kousaku.maron@gmail.com>
2020-09-29 01:30:46 +0900

    fix: add timestamp to entity and repository

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -3,6 +3,8 @@ export type User = {
   name: string
   profile: string
   thumbnailURL: string | null
+  createdAt: Date
+  updatedAt: Date
 }
 
 export const buildUser = (id: string, data: firebase.firestore.DocumentData) => {
@@ -11,6 +13,8 @@ export const buildUser = (id: string, data: firebase.firestore.DocumentData) => 
     name: data.name,
     profile: data.profile,
     thumbnailURL: data.thumbnailURL,
+    createdAt: data.createdAt.toDate(),
+    updatedAt: data.updatedAt.toDate(),
   }
 
   return newUser
