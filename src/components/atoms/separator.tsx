7e5b73aa104f0a3e94c0a1a3128f717e913e8f04
kosaku kurino <kousaku.maron@gmail.com>
2020-09-29 14:10:17 +0900

    feat: add parameter to separator

--- src/components/atoms/separator.tsx
+++ src/components/atoms/separator.tsx
@@ -2,23 +2,37 @@ import React from 'react'
 import { View, ViewProps, StyleSheet } from 'react-native'
 
 type SeparatorProps = ViewProps & {
+  color?: string // html color code
+  width?: number
   layout?: 'horizontal' | 'vertical'
 }
 
-const Separator = ({ layout = 'horizontal', style, ...rest }: SeparatorProps) => {
-  return <View style={[layout === 'horizontal' ? styles.horizontal : styles.vertical, style]} {...rest} />
+const Separator = ({
+  color = '#D3D3D3',
+  width = StyleSheet.hairlineWidth,
+  layout = 'horizontal',
+  style,
+  ...rest
+}: SeparatorProps) => {
+  return (
+    <View
+      style={[
+        layout === 'horizontal'
+          ? { ...styles.horizontal, borderBottomColor: color, borderBottomWidth: width }
+          : { ...styles.vertical, borderRightColor: color, borderRightWidth: width },
+        style,
+      ]}
+      {...rest}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
   horizontal: {
     width: '100%',
-    borderBottomWidth: StyleSheet.hairlineWidth,
-    borderBottomColor: '#D3D3D3',
   },
   vertical: {
     height: '100%',
-    borderRightWidth: StyleSheet.hairlineWidth,
-    borderRightColor: '#D3D3D3',
   },
 })
 
