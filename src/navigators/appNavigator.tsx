3840562cd08fdbf15c973bc94f5c48c3efa91c03
kosaku kurino <kousaku.maron@gmail.com>
2020-11-02 18:52:50 +0900

    fix: add check user data process

--- src/navigators/appNavigator.tsx
+++ src/navigators/appNavigator.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import TabNavigator from './tabNavigator'
 import WelcomeNavigator from './welcomeNavigator'
 import AuthLoadingScreen from '../screens/AuthLoadingScreen'
 import AuthErrorScreen from '../screens/AuthErrorScreen'
 import { auth } from '../repositories/firebase'
+import { useUser } from '../services/hooks/user'
 
-const Stack = createStackNavigator()
+const CheckUserDataNavigator = ({ uid }: { uid: string }) => {
+  const [user, loading, error] = useUser(uid)
+
+  if (loading || (!loading && !user)) {
+    return <AuthLoadingScreen />
+  }
+
+  if (error) {
+    return <AuthErrorScreen />
+  }
+
+  return <TabNavigator />
+}
 
 const AppNavigator = () => {
   const [user, initializing, error] = useAuthState(auth)
@@ -21,7 +33,7 @@ const AppNavigator = () => {
   }
 
   if (user) {
-    return <TabNavigator />
+    return <CheckUserDataNavigator uid={user.uid} />
   }
 
   return <WelcomeNavigator />
